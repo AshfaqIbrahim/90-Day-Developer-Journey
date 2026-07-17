63db4a660321602ca03077b6b3264c14a78a84bf
Ashfaq <113580367+AshfaqIbrahim@users.noreply.github.com>
2026-07-17 23:39:41 +0530

    day-3 sum of array

---
+++ daily-tasks/sumOfArray.js
@@ -0,0 +1,17 @@
+function sumOfArray(arr) {
+  let sum = 0;
+
+  //for loop..
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+
+  // //for of.. loop
+  //   for (let num of arr) {
+  //     sum += num;
+  //   }
+  //   return sum;
+}
+
+console.log(sumOfArray([2, 4, 6, 8]));

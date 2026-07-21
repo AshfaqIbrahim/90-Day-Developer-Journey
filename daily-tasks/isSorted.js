c969a7269420707b785c86e8a71e08d4c6941a7b
Ashfaq <113580367+AshfaqIbrahim@users.noreply.github.com>
2026-07-21 10:57:51 +0530

    Day-7  Problem 14-Check if an array is sorted(w/o .sort())

---
+++ daily-tasks/isSorted.js
@@ -0,0 +1,15 @@
+// Day - 7
+// Problem 14 – Check if an Array is Sorted
+
+
+function isSorted(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 5, 3, 4])); // false

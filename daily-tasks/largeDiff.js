c48a05f97b06728b8b32ca80292dacf4c42dfb24
Ashfaq <113580367+AshfaqIbrahim@users.noreply.github.com>
2026-07-21 10:36:45 +0530

    Day-7 Problem 13: Find the largest difference

---
+++ daily-tasks/largeDiff.js
@@ -0,0 +1,19 @@
+// Day - 7
+// Problem 13: Find the Largest Difference
+
+function largestDiff(arr) {
+  let max = arr[0];
+  let min = arr[0];
+
+  for (let num of arr) {
+    if (num > max) {
+      max = num;
+    }
+    if (num < min) {
+      min = num;
+    }
+  }
+  return max - min;
+}
+
+console.log(largestDiff([8, 3, 10, 2, 6]));

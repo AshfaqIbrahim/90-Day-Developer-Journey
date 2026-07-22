49c266e030885c0e0f03c9aaff076ef4c05ddcfe
Ashfaq <113580367+AshfaqIbrahim@users.noreply.github.com>
2026-07-22 11:41:45 +0530

    Day-8 Problem 15: Rotate an Array by One Position

---
+++ daily-tasks/rotateArr.js
@@ -0,0 +1,13 @@
+// Day  7
+// Problem 15: Rotate an Array by One Position (Right)
+
+function rotateRight(arr) {
+  let last = arr[arr.length - 1];
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = last;
+  return arr;
+}
+console.log(rotateRight([1, 2, 3, 4, 5]));
